a578a7c535b7ef9798d0b56573b47a87a576731c
JoeSheen <joe123sheen@hotmail.com>
2025-10-28 21:20:00 +0000

    Started code refactor

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,6 +1,6 @@
 use aes_gcm::{
     Aes256Gcm, Key, KeyInit, Nonce,
-    aead::{Aead, generic_array::GenericArray},
+    aead::{Aead, generic_array::GenericArray, rand_core::RngCore},
     aes::cipher::typenum,
 };
 use argon2::{
@@ -8,7 +8,6 @@ use argon2::{
     password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng},
 };
 use base64::{Engine, engine::general_purpose};
-use rand::RngCore;
 use zeroize::Zeroize;
 
 const SALT_LEN: usize = 32;
@@ -49,35 +48,32 @@ pub fn verify_master_password(
     result
 }
 
+// TODO: update to use VaultError when required error types are in enum!
 pub fn encrypt(master_password: &str, entry_password: &str) -> Result<String, String> {
-    let salt: [u8; 32] = generate_salt();
+    let salt = generate_salt();
 
-    let argon2: Argon2<'_> = Argon2::default();
-
-    let mut key_bytes: [u8; 32] = [0u8; SALT_LEN];
-    argon2
+    let mut key_bytes = [0u8; 32];
+    Argon2::default()
         .hash_password_into(master_password.as_bytes(), &salt, &mut key_bytes)
-        .map_err(|e| format!("{}", e))?;
+        .map_err(|e| e.to_string())?;
 
-    let key: &GenericArray<u8, _> = Key::<Aes256Gcm>::from_slice(&key_bytes);
-
-    let cipher = Aes256Gcm::new(key);
-
-    let nonce = generate_nonce();
-
-    let entry_ciipher = cipher
-        .encrypt(&nonce, entry_password.as_bytes())
-        .map_err(|e| format!("{}", e))?;
-
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(&key_bytes));
     key_bytes.zeroize();
 
-    let encoded: String = to_base64(entry_ciipher, &salt, nonce.as_slice());
+    let nonce = generate_nonce();
+    let ciphertext = cipher
+        .encrypt(&nonce, entry_password.as_bytes())
+        .map_err(|e| e.to_string())?;
 
-    Ok(encoded)
+    Ok(encode_encrypted_payload(
+        &ciphertext,
+        &salt,
+        nonce.as_slice(),
+    ))
 }
 
 pub fn decrypt(master_password: &str, entry_password: &str) -> Result<String, String> {
-    let (entry_cipher, salt_bytes, nonce_bytes) = from_base64(entry_password)?;
+    let (entry_cipher, salt_bytes, nonce_bytes) = decode_encrypted_payload(entry_password)?;
 
     let argon2: Argon2<'_> = Argon2::default();
 
@@ -103,43 +99,44 @@ pub fn decrypt(master_password: &str, entry_password: &str) -> Result<String, St
 }
 
 fn generate_salt() -> [u8; SALT_LEN] {
-    let mut salt: [u8; 32] = [0u8; SALT_LEN];
-    rand::rng().fill_bytes(&mut salt);
+    let mut salt = [0u8; SALT_LEN];
+    OsRng.fill_bytes(&mut salt);
     salt
 }
 
 fn generate_nonce() -> GenericArray<u8, typenum::U12> {
-    let mut nonce_slice = [0u8; NONCE_LEN];
-    rand::rng().fill_bytes(&mut nonce_slice);
-    GenericArray::clone_from_slice(&nonce_slice)
+    let mut nonce = [0u8; NONCE_LEN];
+    OsRng.fill_bytes(&mut nonce);
+    GenericArray::clone_from_slice(&nonce)
 }
 
-fn to_base64(entry_cipher: Vec<u8>, salt: &[u8], nonce: &[u8]) -> String {
-    let mut encrypted_data: Vec<u8> = Vec::with_capacity(SALT_LEN + NONCE_LEN + entry_cipher.len());
+fn encode_encrypted_payload(ciphertext: &[u8], salt: &[u8], nonce: &[u8]) -> String {
+    let mut encrypted_data = Vec::with_capacity(salt.len() + nonce.len() + ciphertext.len());
     encrypted_data.extend_from_slice(salt);
     encrypted_data.extend_from_slice(nonce);
-    encrypted_data.extend_from_slice(&entry_cipher);
+    encrypted_data.extend_from_slice(ciphertext);
     general_purpose::STANDARD.encode(encrypted_data)
 }
 
-fn from_base64(encoded: &str) -> Result<(Vec<u8>, [u8; SALT_LEN], [u8; NONCE_LEN]), String> {
-    let decoded: Vec<u8> = general_purpose::STANDARD
+fn decode_encrypted_payload(
+    encoded: &str,
+) -> Result<(Vec<u8>, [u8; SALT_LEN], [u8; NONCE_LEN]), String> {
+    let decoded = general_purpose::STANDARD
         .decode(encoded)
-        .map_err(|e| format!("Failed to decode base64 input: {}", e))?;
+        .map_err(|e| e.to_string())?;
 
     if decoded.len() < SALT_LEN + NONCE_LEN {
-        return Err(format!("Decoded data is too short"));
+        return Err("Decoded data is too short".into());
     }
 
-    let salt_bytes: [u8; 32] = decoded[0..SALT_LEN]
-        .try_into()
-        .map_err(|e| format!("Failed to parse salt: {}", e))?;
+    let (salt_bytes, rest) = decoded.split_at(SALT_LEN);
+    let (nonce_bytes, entry_cipher) = rest.split_at(NONCE_LEN);
 
-    let nonce_bytes: [u8; 12] = decoded[SALT_LEN..SALT_LEN + NONCE_LEN]
-        .try_into()
-        .map_err(|e| format!("Failed to parse nonce: {}", e))?;
-
-    let entry_cipher: Vec<u8> = decoded[SALT_LEN + NONCE_LEN..].to_vec();
-
-    Ok((entry_cipher, salt_bytes, nonce_bytes))
+    Ok((
+        entry_cipher.to_vec(),
+        salt_bytes.try_into().map_err(|_| "Failed to parse salt")?,
+        nonce_bytes
+            .try_into()
+            .map_err(|_| "Failed to parse nonce")?,
+    ))
 }
